6aa228d416f80d56bc8bf818e7d655e84c7ae664
PoLLaziY <xlanzerx@gmail.com>
2022-03-10 20:39:03 +0300

    Add data-class - Valute.kt

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.JsonReader
+import android.util.JsonToken
+import android.widget.TextView
+import org.json.JSONObject
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/example/currencyconverter/data/Valute.kt
@@ -0,0 +1,3 @@
+package com.example.currencyconverter.data
+
+data class Valute(val charCode: String, val name: String, var value: Double, var previous: Double)
